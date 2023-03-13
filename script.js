8682d55a04170c050ed04ceb5e56f6b5834cc5f5
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-13 14:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function calculateMinCost() {
+function calculateMinCost(event) {
+	event.preventDefault();
   //your code here
-  
-  
-  
+	const inputEle = document.querySelector('input').value
+    const arr = inputEle.split(',');
+
+	arr.sort(function(a,b){ return a-b; })
+
+	var cost = 0;
+	while (arr.length > 1) {
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.push(res);
+		cont += res;
+		arr.shift();
+		arr.shift();
+		arr.sort(function(a,b){ return a-b; })
+
+		var result = document.getElementById('result').textContent = cost;
+
+	}
+
 }  
